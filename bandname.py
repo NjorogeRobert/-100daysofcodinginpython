0771feb248f9d5b9ee7fd0dd7a0199920ff54131
NjorogeMaina <mainarobertn@gmail.com>
2022-12-10 14:42:11 +0300

    bandname generator

--- bandname.py
+++ bandname.py
@@ -1,3 +1,4 @@
+#a program that generates a band name
 print("Welcome to the Band Name Generator")
 city = input("What's the name of the city you grew up in? ")
 pet = input("What's the name of your pet? ")
